a8bde926cadb9bbeaddbf87df674d8ea3e452e98
samina-shameem <samina.shameem.clt@gmail.com>
2024-12-26 17:26:40 +0100

    avoid func validation,import validate emls and eml eg, dom elemnts,evnt lstnr,

---
+++ js/script.js
@@ -0,0 +1,33 @@
+import { validateEamil } from "./modules/validate.js";
+import { emailExamples } from "./modules/data.js";
+
+//function :validate email
+/*function validateEamil(email){
+    const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}*/
+//console.log("test");
+//dom elements
+const form=document.getElementById('emailForm');
+const emailInput=document.getElementById('emailInput');
+const message=document.getElementById('message');
+
+//event listner
+form.addEventListener('submit',(e) =>{
+    e.preventDefault();//prevent form submission
+
+    //get the email entered
+    const email=emailInput.value.trim();
+    //console.log("testing");
+
+    //validate the email
+    if(validateEamil(email)){
+        message.textContent=`✅"${email}" is a valid email address`;
+        message.style.color='green';
+    }else{
+        message.textContent=`❌"${email}" is not a valid email address.`;
+        message.style.color='red';
+    }
+    //console.log("test2");
+
+});
